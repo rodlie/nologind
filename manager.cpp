9ac9b68d223ac7388ac8165c9da14f82ec46e526
Ole-André Rodlie <ole.andre.rodlie@gmail.com>
2018-10-28 18:34:31 +0100

    wip #1

--- manager.cpp
+++ manager.cpp
@@ -1,9 +1,117 @@
 #include "manager.h"
 #include <QDebug>
+#include <QDBusConnection>
+#include <QDBusMessage>
+#include <QCoreApplication>
+#include <QTimer>
 
 Manager::Manager(QObject *parent) : QObject(parent)
+  , ckit(0)
 {
+    ckit = new QDBusInterface(CONSOLEKIT_SERVICE,
+                              CONSOLEKIT_MANAGER_PATH,
+                              CONSOLEKIT_MANAGER,
+                              QDBusConnection::systemBus(),
+                              this);
+    if (!ckit->isValid()) {
+        qWarning() << "ConsoleKit is not available!!!";
+        QTimer::singleShot(100, qApp, SLOT(quit()));
+    }
+}
 
+bool Manager::canAction(const QString &action)
+{
+    if (!ckit->isValid() || action.isEmpty()) { return false; }
+    QDBusMessage reply = ckit->call(action);
+    qDebug() << "reply for" << action << reply;
+    if (reply.arguments().first().toString() == "yes") { return true; }
+    bool result = reply.arguments().first().toBool();
+    if (!reply.errorMessage().isEmpty()) { result = false; }
+    return result;
+}
+
+const QString Manager::doAction(const QString &action)
+{
+    if (!ckit->isValid() || action.isEmpty()) {
+        return "ConsoleKit is not running or action is empty";
+    }
+    QDBusMessage reply = ckit->call(action, true);
+    return reply.errorMessage();
+}
+
+void Manager::PowerOff(bool trigger)
+{
+    qDebug() << "PowerOff requested" << trigger;
+    if (!trigger || !ckit->isValid()) { return; }
+    QString result = doAction("PowerOff");
+    if (!result.isEmpty()) { qWarning() << result; }
+}
+
+void Manager::Reboot(bool trigger)
+{
+    qDebug() << "Reboot requested" << trigger;
+    if (!trigger || !ckit->isValid()) { return; }
+    QString result = doAction("Reboot");
+    if (!result.isEmpty()) { qWarning() << result; }
+}
+
+void Manager::Suspend(bool trigger)
+{
+    qDebug() << "Suspend requested" << trigger;
+    if (!trigger || !ckit->isValid()) { return; }
+    QString result = doAction("Suspend");
+    if (!result.isEmpty()) { qWarning() << result; }
+}
+
+void Manager::Hibernate(bool trigger)
+{
+    qDebug() << "Hibernate requested" << trigger;
+    if (!trigger || !ckit->isValid()) { return; }
+    QString result = doAction("Hibernate");
+    if (!result.isEmpty()) { qWarning() << result; }
+}
+
+void Manager::HybridSleep(bool trigger)
+{
+    qDebug() << "HybridSleep requested" << trigger;
+    if (!trigger || !ckit->isValid()) { return; }
+    QString result = doAction("HybridSleep");
+    if (!result.isEmpty()) { qWarning() << result; }
+}
+
+const QString Manager::CanPowerOff()
+{
+    qDebug() << "CanPowerOff requested";
+    if (canAction("CanPowerOff")) { return "yes"; }
+    return "no";
+}
+
+const QString Manager::CanReboot()
+{
+    qDebug() << "CanReboot requested";
+    if (canAction("CanReboot")) { return "yes"; }
+    return "no";
+}
+
+const QString Manager::CanSuspend()
+{
+    qDebug() << "CanSuspend requested";
+    if (canAction("CanSuspend")) { return "yes"; }
+    return "no";
+}
+
+const QString Manager::CanHibernate()
+{
+    qDebug() << "CanHibernate requested";
+    if (canAction("CanHibernate")) { return "yes"; }
+    return "no";
+}
+
+const QString Manager::CanHybridSleep()
+{
+    qDebug() << "CanHybridSleep requested";
+    if (canAction("CanHybridSleep")) { return "yes"; }
+    return "no";
 }
 
 const QString Manager::Inhibit(const QString &what,
@@ -11,6 +119,9 @@ const QString Manager::Inhibit(const QString &what,
                                const QString &why,
                                const QString &mode)
 {
+    QString result;
     qDebug() << "Inhibit requested" << what << who << why << mode;
-    return "/tmp/fofo";
+    // https://github.com/rodlie/nologind/issues/2
+    result = "/tmp/fofo"; // whatever, we don't care (for now)
+    return result;
 }
--- manager.h
+++ manager.h
@@ -2,20 +2,169 @@
 #define MANAGER_H
 
 #include <QObject>
+#include <QString>
+#include <QDBusInterface>
+
+#define CONSOLEKIT_SERVICE "org.freedesktop.ConsoleKit"
+#define CONSOLEKIT_MANAGER_PATH "/org/freedesktop/ConsoleKit/Manager"
+#define CONSOLEKIT_MANAGER "org.freedesktop.ConsoleKit.Manager"
 
 class Manager : public QObject
 {
     Q_OBJECT
 public:
     explicit Manager(QObject *parent = NULL);
+/*
+      readwrite b EnableWallMessages = false;
+      readwrite s WallMessage = '';
+      readonly u NAutoVTs = 6;
+      readonly as KillOnlyUsers = [];
+      readonly as KillExcludeUsers = ['root'];
+      readonly b KillUserProcesses = false;
+      readonly b RebootToFirmwareSetup = false;
+      readonly b IdleHint = false;
+      readonly t IdleSinceHint = 0;
+      readonly t IdleSinceHintMonotonic = 0;
+      readonly s BlockInhibited = 'handle-power-key:handle-suspend-key:handle-hibernate-key';
+      readonly s DelayInhibited = 'sleep';
+      readonly t InhibitDelayMaxUSec = 5000000;
+      readonly s HandlePowerKey = 'poweroff';
+      readonly s HandleSuspendKey = 'suspend';
+      readonly s HandleHibernateKey = 'hibernate';
+      readonly s HandleLidSwitch = 'suspend';
+      readonly s HandleLidSwitchDocked = 'ignore';
+      readonly t HoldoffTimeoutUSec = 30000000;
+      readonly s IdleAction = 'ignore';
+      readonly t IdleActionUSec = 1800000000;
+      readonly b PreparingForShutdown = false;
+      readonly b PreparingForSleep = false;
+      readonly (st) ScheduledShutdown = ('', 0);
+      readonly b Docked = false;
+*/
+
+private:
+    QDBusInterface *ckit;
 
 signals:
+/*
+      SessionNew(s session_id,
+                 o object_path);
+      SessionRemoved(s session_id,
+                     o object_path);
+      UserNew(u uid,
+              o object_path);
+      UserRemoved(u uid,
+                  o object_path);
+      SeatNew(s seat_id,
+              o object_path);
+      SeatRemoved(s seat_id,
+                  o object_path);
+      PrepareForShutdown(b start);
+      PrepareForSleep(b start);
+*/
+
+private slots:
+    bool canAction(const QString &action);
+    const QString doAction(const QString &action);
 
 public slots:
+    void PowerOff(bool trigger);
+    void Reboot(bool trigger);
+    void Suspend(bool trigger);
+    void Hibernate(bool trigger);
+    void HybridSleep(bool trigger);
+    const QString CanPowerOff();
+    const QString CanReboot();
+    const QString CanSuspend();
+    const QString CanHibernate();
+    const QString CanHybridSleep();
     const QString Inhibit(const QString &what,
                           const QString &who,
                           const QString &why,
                           const QString &mode);
+/*
+GetSession(in  s session_id,
+                 out o object_path);
+      GetSessionByPID(in  u pid,
+                      out o object_path);
+      GetUser(in  u uid,
+              out o object_path);
+      GetUserByPID(in  u pid,
+                   out o object_path);
+      GetSeat(in  s seat_id,
+              out o object_path);
+      ListSessions(out a(susso) session_list);
+      ListUsers(out a(uso) user_list);
+      ListSeats(out a(so) seat_list);
+      ListInhibitors(out a(ssssuu) inhibitor_list);
+      CreateSession(in  u uid,
+                    in  u pid,
+                    in  s service,
+                    in  s type,
+                    in  s class,
+                    in  s desktop,
+                    in  s seat_id,
+                    in  u vtnr,
+                    in  s tty,
+                    in  s display,
+                    in  b remote,
+                    in  s remote_user,
+                    in  s remote_host,
+                    in  a(sv) properties,
+                    out s session_id,
+                    out o object_path,
+                    out s runtime_path,
+                    out h fifo_fd,
+                    out u uid,
+                    out s seat_id,
+                    out u vtnr,
+                    out b existing);
+      ReleaseSession(in  s session_id);
+      ActivateSession(in  s session_id);
+      ActivateSessionOnSeat(in  s session_id,
+                            in  s seat_id);
+      LockSession(in  s session_id);
+      UnlockSession(in  s session_id);
+      LockSessions();
+      UnlockSessions();
+      KillSession(in  s session_id,
+                  in  s who,
+                  in  i signo);
+      KillUser(in  u uid,
+               in  i signo);
+      TerminateSession(in  s session_id);
+      TerminateUser(in  u uid);
+      TerminateSeat(in  s seat_id);
+      SetUserLinger(in  u uid,
+                    in  b linger,
+                    in  b interactive);
+      AttachDevice(in  s seat_id,
+                   in  s sysfs,
+                   in  b interactive);
+      FlushDevices(in  b interactive);
+[X]   PowerOff(in  b interactive);
+[X]   Reboot(in  b interactive);
+[X]   Suspend(in  b interactive);
+[X]   Hibernate(in  b interactive);
+[X]   HybridSleep(in  b interactive);
+[X]   CanPowerOff(out s result);
+[X]   CanReboot(out s result);
+[X]   CanSuspend(out s result);
+[X]   CanHibernate(out s result);
+[X]   CanHybridSleep(out s result);
+      ScheduleShutdown(in  s type,
+                       in  t usec);
+      CancelScheduledShutdown(out b cancelled);
+[*]   Inhibit(in  s what,
+              in  s who,
+              in  s why,
+              in  s mode,
+              out h pipe_fd);
+      CanRebootToFirmwareSetup(out s result);
+      SetRebootToFirmwareSetup(in  b enable);
+      SetWallMessage(in  s wall_message,
+                     in  b enable);
+*/
 };
 
 #endif // MANAGER_H
